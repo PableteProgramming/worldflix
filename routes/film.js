93fad183e0a067024126c954fd67fe5721d21c48
PableteProgramming <70910514+PableteProgramming@users.noreply.github.com>
2022-04-02 20:43:29 +0200

    Uploading film and image successfully

--- routes/film.js
+++ routes/film.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const Film = require('../models/film')
 const fs = require('fs')
+const fileUpload = require('express-fileupload')
+
+
+const path = require('path')
 
 const router = express.Router()
+router.use(fileUpload())
 
 //showing films => back to home
 router.get("/", (req, res) => {
@@ -34,25 +39,54 @@ router.post("/", async(req, res) => {
         name: req.body.name,
         tags: farr
     })
-    try {
-        newFilm = await newFilm.save()
-        let p = "public/uploads";
-        let f = p + "/films";
-        let path = f + "/" + newFilm.id;
-        fs.mkdirSync(p)
-        fs.mkdirSync(f)
-        fs.mkdirSync(path)
-        fs.writeFileSync(path + "/empty.txt", "nothing")
-        res.render("film/show", {
-            film: newFilm
-        })
-    } catch (e) {
-        res.redirect("/")
+
+    let filmFile = req.files.film;
+    if (path.extname(filmFile.name) == ".mp4") {
+        let image = req.files.image;
+        try {
+            newFilm = await newFilm.save()
+            let p = "public/uploads"
+            let f = p + "/films"
+            let path = f + "/" + newFilm.id
+            try {
+                fs.mkdirSync(p)
+
+            } catch (e) {
+                console.log(e)
+            }
+            try {
+                fs.mkdirSync(f)
+
+            } catch (e) {
+                console.log(e)
+            }
+            fs.mkdirSync(path)
+            fs.writeFileSync(path + "/empty.txt", "nothing")
+            filmFile.mv(path + "/film.mp4", (err) => {
+                if (err) {
+                    res.send("an error occured while uploading...")
+                }
+            })
+            image.mv(path + "/img.png", (err) => {
+                if (err) {
+                    res.send("an error occured while uploading...")
+                }
+            })
+            res.render("film/show", {
+                film: newFilm
+            })
+        } catch (e) {
+            console.log(e)
+            res.redirect("/")
+        }
+    } else {
+        res.send("We can only upload mp4 files")
     }
 })
 
 router.post("/remove/:id", async(req, res) => {
     await Film.findByIdAndDelete(req.params.id)
+    fs.rmdirSync("public/uploads/films/" + req.params.id, { recursive: true })
     res.redirect("/")
 })
 
